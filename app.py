6b20107ea0fe9a3b652005c6c19f094bf6e0396d
ucyo <cayoglu@me.com>
2020-05-21 15:35:18 +0200

    Add functions for calling search urls for several second hand shops
    - Booklooker.de 
      - Works as expected, thanks to ISBN search.
      - If the book was never on the side it returns no information.
      - If it was on sale previous it will show the average price.
      - Otherwise no comments.
    - eBay Kleinanzeigen
      - Needs to be sanitized, search is based on title.
      - Some queries will fail gloriously e.g. Harry Potter 7
    - Amazon.de
      - Works as expected, thanks to ISBN search.
    - Rebuy.de
      - Needs to be sanitized, search is based on title.

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ from flask import Flask, jsonify, redirect, escape
 app = Flask(__name__)
 GRC = get_gr_client()
 
+
+# TODO all these need to be sanitized
+# e.g. Harry Potter 7 does not work with ebay
+# http://localhost:5000/api/v1/book/show/136251.Harry_Potter_and_the_Deathly_Hallows
+def get_booklooker_search_url(book):
+    return "https://www.booklooker.de/B%C3%BCcher/Angebote/isbn={}".format(book.isbn13)
+def get_ebay_kleinanzeigen_search_url(book):
+    return "https://www.ebay-kleinanzeigen.de/s-musik-film-buecher/{}/k0c73".format(escape(book.title.replace(" ", "-")))
+def get_amazon_de_search_url(book):
+    return "https://www.amazon.de/s?i=stripbooks&rh=p_66%3A{}".format(book.isbn13)
+def get_rebuy_search_url(book):
+    return "https://www.rebuy.de/kaufen/suchen?q={}&c=91".format(escape(book.title.replace(" ", "%20")))
+
 # TODO connect this to the API
 @app.route('/book/show/<int:goodreads_id><garbage>')
 def to_booklooker(goodreads_id, garbage):
