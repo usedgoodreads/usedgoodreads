15b3cf3c51390737bd02687040c3a25a9829d401
ucyo <cayoglu@me.com>
2020-05-21 15:40:27 +0200

    Add API for book information including search Urls
    - Currently setup to use `/api/v1/...` as API call.

--- app.py
+++ app.py
@@ -17,6 +17,27 @@ def get_amazon_de_search_url(book):
 def get_rebuy_search_url(book):
     return "https://www.rebuy.de/kaufen/suchen?q={}&c=91".format(escape(book.title.replace(" ", "%20")))
 
+
+@app.route('/api/v1/book/show/<int:goodreads_id><garbage>')
+def book_information(goodreads_id, garbage):
+    """API address to return minimal book information.
+    """
+    book = GRC.book(goodreads_id)
+
+    booklooker_search_url = get_booklooker_search_url(book)
+    ebay_kleinanzeigen_search_url = get_ebay_kleinanzeigen_search_url(book)
+    amazon_de_search_url = get_amazon_de_search_url(book)
+    rebuy_search_url = get_rebuy_search_url(book)
+
+    json = dict(title=book.title,
+                isbn=book.isbn13,
+                rating=book.average_rating,
+                booklooker_search_url=booklooker_search_url,
+                ebay_kleinanzeigen_search_url=ebay_kleinanzeigen_search_url,
+                amazon_de_search_url=amazon_de_search_url,
+                rebuy_search_url=rebuy_search_url)
+    return jsonify(json)
+
 # TODO connect this to the API
 @app.route('/book/show/<int:goodreads_id><garbage>')
 def to_booklooker(goodreads_id, garbage):
