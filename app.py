cdd9251a2c35ae35be362ad8197d0220e739051c
ucyo <cayoglu@me.com>
2020-05-21 15:32:57 +0200

    Redirect to booklooker.de for search

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from grclient import get_gr_client
+from flask import Flask, jsonify, redirect, escape
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+GRC = get_gr_client()
+
+# TODO connect this to the API
+@app.route('/book/show/<int:goodreads_id><garbage>')
+def to_booklooker(goodreads_id, garbage):
+    """Redirect to booklooker search.
+    """
+    book = GRC.book(goodreads_id)
+    booklooker_search_url = "https://www.booklooker.de/B%C3%BCcher/Angebote/isbn={}".format(book.isbn13)
+    return redirect(booklooker_search_url)
