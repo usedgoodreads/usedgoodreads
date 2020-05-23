fd22d382f2987236fd356295ec1d8ad67f74cb82
ucyo <cayoglu@me.com>
2020-05-23 15:23:29 +0200

    Move eBay Kleinanzeigen into Dataclass
    This structure should be valid for all shops. A single function to
    transform each search result into a BookOffer and another function
    to generate a list of BookOffers

--- backend/kleinanzeigen.py
+++ backend/kleinanzeigen.py
@@ -1,51 +1,57 @@
 import time
 import random
 
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
+from dataclasses import dataclass
+from datamodels.book import Book, BookOffer
 
-options = Options()
-options.add_argument("--headless")
+def main():
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
 
-driver = webdriver.Firefox(options=options)
+    book = Book(title = "Herr der Ringe", ean = "978-3-86680-192-9")
+    offers = EbayKleinanzeigen(book).get_offers(driver)
+    print(offers)
+    driver.close()
 
-driver.get("https://www.ebay-kleinanzeigen.de/s-buecher-zeitschriften/c76")  # books
+@dataclass
+class EbayKleinanzeigen:
+    book: Book
 
-gdpr = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "gdpr-banner-accept")))
-gdpr.click()
+    def single_searchresult_to_bookoffer(self, item):
+        main = item.find_element_by_class_name("aditem-main")
+        a = main.find_element_by_tag_name("a")
+        title = a.text
+        link = a.get_attribute("href")
+        desc = main.find_element_by_tag_name("p").text
 
-search = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "site-search-query")))
-search.send_keys("Harry Potter")
-search.send_keys(Keys.RETURN)
+        details = item.find_element_by_class_name("aditem-details")
+        price = details.find_element_by_tag_name("strong").text
+        date = item.find_element_by_class_name("aditem-addon").text
+        return BookOffer(title=title, link=link, description=desc, price=price, creation_date=date)
 
-time.sleep(5)
+    def get_offers(self, driver):
+        driver.get("https://www.ebay-kleinanzeigen.de/s-buecher-zeitschriften/c76")  # books
+        gdpr = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "gdpr-banner-accept")))
+        gdpr.click()
 
-results = driver.find_elements_by_id("srchrslt-adtable")
-assert len(results) == 1
-result = results[0]
-assert result.tag_name == "ul"
+        search = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "site-search-query")))
+        search.send_keys(self.book.title)
+        search.send_keys(Keys.RETURN)
 
-for item in result.find_elements_by_class_name("lazyload-item"):
-    main = item.find_element_by_class_name("aditem-main")
-    a = main.find_element_by_tag_name("a")
-    print(a.text)
-    print(a.get_attribute("href"))
-    desc = main.find_element_by_tag_name("p").text
-    print(desc)
+        time.sleep(5)
+        results = driver.find_elements_by_id("srchrslt-adtable")
+        assert len(results) == 1
+        result = results[0]
+        assert result.tag_name == "ul"
+        return [self.single_searchresult_to_bookoffer(item)
+                for item in result.find_elements_by_class_name("lazyload-item")]
 
-    details = item.find_element_by_class_name("aditem-details")
-    price = details.find_element_by_tag_name("strong").text
-    print(price)
-
-    date = item.find_element_by_class_name("aditem-addon").text
-    print(date)
-
-    print()
-
-
-driver.close()
+if __name__ == '__main__':
+    main()
