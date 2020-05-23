5c724902b1c13c5b310b69a731c46f11a626179a
ucyo <cayoglu@me.com>
2020-05-23 15:20:20 +0200

    Move to folder and add BookOffer class

--- backend/datamodels/book.py
+++ backend/datamodels/book.py
@@ -113,5 +113,15 @@ class Book:
         check = self._compute_isbn10_checksum(chars)
         return "{}-{}-{}-{}".format(chars[0],"".join(chars[1:4]), "".join(chars[4:9]), check)
 
+
+@dataclass
+class BookOffer:
+    title: str
+    link: str
+    description: str
+    price: str
+    creation_date: str
+
+
 if __name__ == '__main__':
     main()
