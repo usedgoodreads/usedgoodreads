920c0fa6cbde87c6f0e03ac2017b76ecc93cd920
daniel-j-h <daniel@trvx.org>
2020-05-24 15:48:42 +0200

    Adds frontend skeleton

---
+++ frontend/usedgoodreads/App.tsx
@@ -0,0 +1,19 @@
+import { h, Component } from 'preact';
+
+
+export interface Props {
+  // pass
+};
+
+interface State {
+  // pass
+};
+
+
+export class App extends Component<Props, State> {
+  public render(props: Props, state: State) {
+    return (
+      <h1>It works!</h1>
+    );
+  }
+}
